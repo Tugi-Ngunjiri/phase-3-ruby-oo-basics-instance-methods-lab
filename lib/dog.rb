15f944277a7705cdd161fa990fd09a7b46dd73ef
tugi <blueqwinn@gmail.com>
2023-02-08 11:38:17 +0300

    first commit

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    #Class body
+
+#Instance Method Definition
+def bark
+    puts "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.bark #=> "Woof!"
+
+
+snoopy = Dog.new
+snoopy.bark #=> "Woof!"
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Talk 
+     def person 
+        puts "Hello World!"
+         end
+      end
+     person.talks #=> "Says Hello World!"
+
+     class Walk
+        def person
+            puts "The Person is walking".
+            person.walks #=> "The person is walking"
+        end
+      end
+        person.walks #=> "The person is walking"
